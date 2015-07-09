3c944421478de7b5373b214595d58656cab50e4e
Jacob Brenneman <j.brenneman19@gmail.com>
2015-07-08 22:27:23 -0600

    homepage started with pop-out video display and working links

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+// Store Page
+router.get('/views/store', function (req, res, next) {
+  res.render('store');
+});
+// About Page
+router.get('/views/about', function (req, res, next) {
+  res.render('about');
+});
+
 
 module.exports = router;
